ea56078395526fa686144bae104086896233abd6
Tomas Sivicki <tomas.sivicki@gmail.com>
2026-08-02 00:39:55 +0200

    style: reduce button corner radius

--- blog/src/components/ui/button.tsx
+++ blog/src/components/ui/button.tsx
@@ -6,7 +6,7 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'font-jersey inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-[4px] text-sm transition-colors focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  'font-jersey inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-[2px] text-sm transition-colors focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
   {
     variants: {
       variant: {
