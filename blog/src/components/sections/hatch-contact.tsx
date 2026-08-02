bd597d973ed586c5ff91cc44097e1715a85af93f
Tomas Sivicki <tomas.sivicki@gmail.com>
2026-08-02 11:50:01 +0200

    copy: clarify connections message

--- blog/src/components/sections/hatch-contact.tsx
+++ blog/src/components/sections/hatch-contact.tsx
@@ -123,7 +123,7 @@ export default function HatchContact({
                 >
                   Ask around
                 </a>
-                , my awesome collegues at these best companies
+                , I met awesome people through my work with these great companies
               </span>
             </div>
           </div>
